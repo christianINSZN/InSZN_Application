43d471ffae635f58a5a99198df1721b913896df1
christianINSZN <cmberry1@me.com>
2025-09-25 11:55:09 -0500

    QB Passing Analytics Mobile Optimization

--- src/components/playerprofiles/QB/PassingAnalytics/PocketProduction.js
+++ src/components/playerprofiles/QB/PassingAnalytics/PocketProduction.js
@@ -290,7 +290,7 @@ const PocketProduction = ({ playerId, year, weeklyGrades, teamGames, allPlayerPe
                 onClick={() => toggleDropdown(metric.id)}
                 className="ml-2 text-gray-500 hover:text-gray-700 focus:outline-none"
               >
-                <svg className="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="h-4 w-4 mt-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
               </button>
