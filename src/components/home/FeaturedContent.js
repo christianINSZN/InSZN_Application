3127cd3e521802526ad3829399e79272b1caa5fe
christianINSZN <cmberry1@me.com>
2025-10-31 11:43:27 -0500

    Homepage Update

--- src/components/home/FeaturedContent.js
+++ src/components/home/FeaturedContent.js
@@ -15,21 +15,21 @@ function FeaturedContent() {
     },
     {
       image: '/Home_3.jpg',
-      videoUrl: 'https://www.youtube.com/watch?v=s_0oX0rDYdc',
-      title: 'Oklahoma Football: Keys for John Mateer vs Tennessee in Week 10',
-      desc: 'We discuss several keys for John Mateer to find success against Tennessee\'s defense in this major SEC Week 10 contest. '
+      videoUrl: 'https://www.youtube.com/watch?v=jX1Icc6fDLE',
+      title: 'Vanderbilt vs. Texas: INSZN\'s Week 10 Scouting Report',
+      desc: 'We preview the Vanderbilt Commodores matchup against the Texas Longhorns and discuss how the Longhorns\' defense can slow down Diego Pavia and the Commodores\' offense, as well as what to expect out of Arch Manning and the Longhorns\' offense in Austin on Saturday.'
     },
     {
       image: '/Home_4.jpg',
-      videoUrl: 'https://www.youtube.com/watch?v=x2eLQmUMQy4',
-      title: 'Tennessee Football: Keys for Joey Aguilar vs Oklahoma in Week 10',
-      desc: 'We discuss several keys for Joey Aguilar to find success against OU\'s defense in this major SEC Week 10 contest. '
+      videoUrl: 'https://www.youtube.com/watch?v=x8eJuyOMSkQ',
+      title: 'Georgia vs. Florida: INSZN\'s Week 10 Scouting Report',
+      desc: 'We preview a premiere SEC Week 10 contest and a rivalry game as the Georgia Bulldogs traveling to Florida to take on the Florida Gators.  We discuss the vaunted Georgia defense, how DJ Lagway and the Gators\' offense need to sustain drives and protect the ball, and how the advanced analytics show that this could be a much closer game than many expect.'
     },
     {
       image: '/Home_5.jpg',
-      videoUrl: 'https://www.youtube.com/watch?v=ktPVqHoYrIQ',
-      title: 'Alabama Football: Ty Simpson\'s PERFORMANCE vs South Carolina, Reaction & RecapTy Simpson',
-      desc: 'We breakdown what stood out in Ty\'s performance and how he was able to keep on even keel and lead the Crimson Tide to a come-from-behind victory in Week 9.'
+      videoUrl: 'https://www.youtube.com/watch?v=Kxx0Ao78bIg',
+      title: 'Miami vs. SMU: INSZN\'s Week 10 Scouting Report',
+      desc: 'We preview the a quality ACC Week 10 matchup as the Miami Hurricanes travel to Dallas to take on the SMU Mustangs.  We discuss the efficiency by which Miami has played on both sides of the ball this year, and how Miami can come out of Dallas with a victory.'
     },
   ];
 
