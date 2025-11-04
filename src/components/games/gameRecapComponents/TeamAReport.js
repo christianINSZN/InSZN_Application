6e0fc298822ffd04b4638365d6921f33ed438fb3
christianINSZN <cmberry1@me.com>
2025-11-03 22:30:37 -0600

    Homepage Game Update

--- src/components/games/gameRecapComponents/TeamAReport.js
+++ src/components/games/gameRecapComponents/TeamAReport.js
@@ -32,7 +32,7 @@ const TeamAReport = ({ teamName, teamId, year, gameId, gameStats }) => {
   const metrics = [
     {
       key: 'scoring_opportunities_opportunities',
-      label: 'TEST TEST',
+      label: 'Scoring Opportunities',
       tooltip: {
         title: 'Scoring Opportunities',
         definition: 'Number of offensive drives reaching at least the opponent’s 40-yard line.',
