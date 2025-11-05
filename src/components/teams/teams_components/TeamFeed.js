10999ccb74f5caef6ca6345e6af4b4c1038c5d42
christianINSZN <cmberry1@me.com>
2025-11-05 15:48:44 -0600

    Homepage Game Update

--- src/components/teams/teams_components/TeamFeed.js
+++ src/components/teams/teams_components/TeamFeed.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const TeamFeed = ({ teamData, year }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.commento.io/js/commento.js';
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   if (!teamData?.id) return null;
 
   return (
-    <div className="h-[300px] overflow-auto bg-white rounded-lg p-2">
+    <div className="h-[300px] overflow-auto bg-white rounded-lg p-4">
       <div
         id="commento"
         data-page-id={`/teams/${teamData.id}/${year}`}
-        dangerouslySetInnerHTML={{
-          __html: `<script defer src="https://cdn.commento.io/js/commento.js"></script>`,
-        }}
       />
     </div>
   );
