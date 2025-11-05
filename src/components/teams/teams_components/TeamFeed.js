8023453dadc55d51be6d0caa47cfced2aefdc062
christianINSZN <cmberry1@me.com>
2025-11-05 15:37:23 -0600

    Homepage Game Update

--- src/components/teams/teams_components/TeamFeed.js
+++ src/components/teams/teams_components/TeamFeed.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const TeamStats = ({ teamData }) => {
-  const [tweets, setTweets] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-    return (
-    <div className="p-0 shadow-xl rounded-lg h-[full]">
-      <div className="h-[300px] flex items-center justify-center bg-gray-100 rounded-lg">
-        <p className="text-[#235347] text-xl text-center">TEAM FEED COMING SOON</p>
-      </div>
+  return (
+    <div className="p-0 shadow-xl rounded-lg h-full">
+      <div
+        className="h-[300px] overflow-auto bg-gray-100 rounded-lg p-4"
+        dangerouslySetInnerHTML={{
+          __html: `
+            <div id="commento" data-page-id="team-${teamData.id}"></div>
+            <script defer src="https://cdn.commento.io/js/commento.js"></script>
+          `,
+        }}
+      />
     </div>
   );
-}
+};
 
 export default TeamStats;
