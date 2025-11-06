7d407d1f4580389b2ae05512e321fb8c1e9eddcb
christianINSZN <cmberry1@me.com>
2025-11-06 11:20:24 -0600

    Poll Test

--- src/components/home/LeftSidebar.js
+++ src/components/home/LeftSidebar.js
@@ -1,12 +1,12 @@
 // home/LeftSidebar.jsx
 import TopTeams from './TopTeams';
 import FeaturedVideos from './FeaturedVideos';
-import DailyPoll from './DailyPoll'; // <-- Add this import
+import PollWidget from './DailyPoll'; // <-- Add this import
 
 function LeftSidebar() {
   return (
     <div className="space-y-6">
-      <DailyPoll />          {/* <-- Poll appears first */}
+      <PollWidget slug="heisman" />
       <TopTeams />
     </div>
   );
